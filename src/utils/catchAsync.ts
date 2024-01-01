4a6e76f7866acf999dcbcf223dc6f81e3f26908f
nishargshah_openxcell <nisharg.shah@openxcell.com>
2024-01-01 20:31:39 +0530

    feat: new boolean added for pagination in catchAsync

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,6 +1,6 @@
 import { throwAxiosError } from '@/api/utils';
 
-import type { AxiosSignal } from '@/types/axios';
+import type { AxiosSignal, DocsOutput } from '@/types/axios';
 import type { Promisable } from 'type-fest';
 
 type DefaultParamsInput = AxiosSignal;
@@ -9,19 +9,24 @@ interface CatchAsyncOptions {
   throwError?: boolean;
 }
 
-type CatchAsyncInput<T, R> = (data: T, options?: CatchAsyncOptions) => Promise<R>;
+type CatchAsyncOutputCond<T, P> = P extends true ? DocsOutput<T> : P extends false ? T[] : T;
 
-type CatchAsyncOutput<T, R, E> = (
+type CatchAsyncInput<T, R, P> = (
   data: T,
   options?: CatchAsyncOptions
-) => Promise<R | E | undefined>;
+) => Promise<CatchAsyncOutputCond<R, P>>;
+
+type CatchAsyncOutput<T, R, P, E> = (
+  data: T,
+  options?: CatchAsyncOptions
+) => Promise<CatchAsyncOutputCond<R, P> | E | undefined>;
 
 type ErrorCB<E> = (err: unknown) => Promisable<E>;
 
-type CatchAsync = <I, R, E = undefined, T = I & DefaultParamsInput>(
-  fn: CatchAsyncInput<T, R>,
+type CatchAsync = <I, R, P = unknown | boolean, E = undefined, T = I & DefaultParamsInput>(
+  fn: CatchAsyncInput<T, R, P>,
   errorCB?: ErrorCB<E>
-) => CatchAsyncOutput<T, R, E>;
+) => CatchAsyncOutput<T, R, P, E>;
 
 const catchAsync: CatchAsync =
   (fn, errorCB = undefined) =>
