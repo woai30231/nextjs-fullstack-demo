a3fa3dc620390f68f74c269c58c3ecc0be8ea54c
Nisharg Shah <nishargshah3101@gmail.com>
2024-05-19 21:30:53 +0530

    feat: not found route added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+import NotFound from '@/components/defaults/notFound/NotFound';
+
+export default NotFound;
---
+++ src/components/defaults/notFound/NotFound.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import styles from '@/components/defaults/notFound/NotFound.module.css';
+
+import type { Component } from '@/types';
+
+const NotFound: Component = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className={styles.notFound}>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource &quot;{pathname}&quot;</p>
+      <Link href="/">
+        <button type="button">Return Home</button>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
