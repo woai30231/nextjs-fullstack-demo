23c1b8958ba45546dbcc34be58b87f53c66231c3
Nisharg Shah <nishargshah3101@gmail.com>
2025-03-10 02:36:53 +0530

    feat: loading slice readded

---
+++ src/store/slices/loading/loading.slice.ts
@@ -0,0 +1,17 @@
+import type {
+  CreateLoadingSlice,
+  LoadingSliceInitialState,
+} from '@/store/slices/loading/loading.type';
+
+const initialState: LoadingSliceInitialState = {
+  isLoading: true,
+};
+
+const createLoadingSlice: CreateLoadingSlice = (set) => ({
+  ...initialState,
+  setLoading: (payload) => {
+    set({ isLoading: payload }, false, 'loading/setLoading');
+  },
+});
+
+export default createLoadingSlice;
