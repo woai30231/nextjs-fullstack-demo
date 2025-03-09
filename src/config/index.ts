7da35917740aa7cba84601b9bc6ff887bde353d2
Nisharg Shah <nishargshah3101@gmail.com>
2025-03-10 03:36:22 +0530

    fix: baseurl issue fixed

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ type Environment = 'development' | 'production';
 const config = {
   NODE_ENV: process.env.NODE_ENV as Environment,
   ENV_TYPE: process.env.NODE_ENV as Environment,
-  NEXT_PUBLIC_API_PATH: process.env.NEXT_PUBLIC_API_PATH as string,
+  NEXT_PUBLIC_API_PATH: `${globalThis.origin}/api`,
 } as const;
 
 export default config;
