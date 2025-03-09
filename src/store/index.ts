8e34abdbd3454abc38a001d4e1918be915a5e1b0
Nisharg Shah <nishargshah3101@gmail.com>
2025-03-10 02:15:54 +0530

    feat: loading slice removed

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,20 +3,16 @@ import { devtools } from 'zustand/middleware';
 
 import { useZustand } from '@/context/ZustandProvider';
 import createAuthSlice from '@/store/slices/auth/auth.slice';
-import createLoadingSlice from '@/store/slices/loading/loading.slice';
 import createThemeSlice from '@/store/slices/theme/theme.slice';
 
 import type { CreateStore, StateFromFunctions, UseStore } from '@/types/store.type';
 
-export type StoreState = StateFromFunctions<
-  [typeof createAuthSlice, typeof createLoadingSlice, typeof createThemeSlice]
->;
+export type StoreState = StateFromFunctions<[typeof createAuthSlice, typeof createThemeSlice]>;
 
 export const createStore: CreateStore = (state) =>
   createZustandStore<StoreState>()(
     devtools((...a) => ({
       ...createAuthSlice(...a),
-      ...createLoadingSlice(...a),
       ...createThemeSlice(...a),
       ...state,
     })),
