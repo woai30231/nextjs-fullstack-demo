c9a432dc807b07b4290de1019c743be828c8cda3
Nisharg Shah <nishargshah3101@gmail.com>
2025-03-30 22:32:00 +0530

    fix: script sh file changed

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+forCheck="--for-check"
+isForCheck="$([[ $1 == $forCheck ]] && echo 0 || echo 1)"
+
 echo "Started"
 
-pnpm --silent format:fix --log-level silent
-echo "Prettier Completed"
-pnpm --silent lint:fix
-echo "Eslint Completed"
-pnpm --silent typecheck
-echo "Typescript Completed"
+if [ "$isForCheck" -eq 0 ]; then
+  npm run --silent format:check -- --log-level silent
+  echo "Prettier Checked"
+
+  npm run --silent lint
+  echo "ESLint Checked"
+
+  npm run --silent typecheck
+  echo "TypeScript Checked"
+else
+  npm run --silent format:fix -- --log-level silent
+  echo "Prettier Completed"
+
+  npm run --silent lint:fix
+  echo "ESLint Completed"
+
+  npm run --silent typecheck
+  echo "TypeScript Completed"
+fi
 
 echo "Done"
