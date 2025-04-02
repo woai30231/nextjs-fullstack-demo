46a40befd085d6e2c51c6f9f3c1c06359bcab4be
Nisharg Shah <nishargshah3101@gmail.com>
2025-04-03 02:35:45 +0530

    feat: airbnb js package added for linting

--- lint/react.eslint.mjs
+++ lint/react.eslint.mjs
@@ -1,54 +1,55 @@
 import ESLintPluginNext from '@next/eslint-plugin-next';
 import ESLintPluginQuery from '@tanstack/eslint-plugin-query';
+import ESLintConfigAirbnbExtended from 'eslint-config-airbnb-extended';
 import ESLintPluginReactRefresh from 'eslint-plugin-react-refresh';
 
 const customReactESLintConfig = [
   // AIRBNB CONFIG
   // IT IS USING IMPORT, REACT, REACT HOOKS (ALL) & JSX A11Y INTERNALLY SO THOSE 4 PACKAGES SHOULD BE AFTER THIS
-  // 'airbnb',
-  // AIRBNB HOOKS CONFIG
-  // 'airbnb/hooks',
+  ...ESLintConfigAirbnbExtended.configs.recommended,
   // REACT CONFIG RULES
-  // {
-  //   rules: {
-  //     'react/function-component-definition': [
-  //       'error',
-  //       {
-  //         namedComponents: 'arrow-function',
-  //       },
-  //     ],
-  //     'react/prop-types': 'off',
-  //     'react/jsx-props-no-spreading': 'off',
-  //     'react/jsx-fragments': ['error', 'element'],
-  //     'react/require-default-props': [
-  //       'error',
-  //       {
-  //         functions: 'defaultArguments',
-  //       },
-  //     ],
-  //   },
-  // },
-  // // JSX A11Y CONFIG RULES
-  // {
-  //   rules: {
-  //     'jsx-a11y/label-has-associated-control': [
-  //       'error',
-  //       {
-  //         required: {
-  //           some: ['nesting', 'id'],
-  //         },
-  //       },
-  //     ],
-  //     'jsx-a11y/label-has-for': [
-  //       'error',
-  //       {
-  //         required: {
-  //           some: ['nesting', 'id'],
-  //         },
-  //       },
-  //     ],
-  //   },
-  // },
+  {
+    name: 'react/rules',
+    rules: {
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: 'arrow-function',
+        },
+      ],
+      'react/prop-types': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/jsx-fragments': ['error', 'element'],
+      'react/require-default-props': [
+        'error',
+        {
+          functions: 'defaultArguments',
+        },
+      ],
+    },
+  },
+  // JSX A11Y CONFIG RULES
+  {
+    name: 'jsx-a11y/rules',
+    rules: {
+      'jsx-a11y/label-has-associated-control': [
+        'error',
+        {
+          required: {
+            some: ['nesting', 'id'],
+          },
+        },
+      ],
+      'jsx-a11y/label-has-for': [
+        'error',
+        {
+          required: {
+            some: ['nesting', 'id'],
+          },
+        },
+      ],
+    },
+  },
   // REACT REFRESH CONFIG
   {
     name: 'react-refresh/rules',
