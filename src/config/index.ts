5899514b6ed01bec67bf20253bf6573ccab44174
Nisharg Shah <nishargshah3101@gmail.com>
2025-04-10 22:53:36 +0530

    fix: url issue

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ type Environment = 'development' | 'production';
 const config = {
   NODE_ENV: process.env.NODE_ENV as Environment,
   ENV_TYPE: process.env.NODE_ENV as Environment,
-  NEXT_PUBLIC_API_PATH: `${globalThis.origin}/api`,
+  NEXT_PUBLIC_API_PATH: process.env.NEXT_PUBLIC_API_PATH as string,
 } as const;
 
 export default config;
